8a7a06cc8b7fcc61e7258c340362089344a54bfa
Alek Sharma <aleksharma12@gmail.com>
2016-08-24 12:32:33 -0700

    Edits for Solution 3

--- book/analysis/anagrams.py
+++ book/analysis/anagrams.py
@@ -90,23 +90,23 @@ of the sorting process.
 Solution 3: Brute Force
 -----------------------
 
-A *brute force* technique for solving a problem typically tries to
-exhaust all possibilities. For the anagram detection problem, we can
-simply generate a list of all possible strings using the characters from
-`s1` and then see if `s2` occurs. However, there is a difficulty with
-this approach. When generating all possible strings from `s1`, there are
-$$n$$ possible first characters, $$n-1$$ possible characters for the second
-position, $$n-2$$ for the third, and so on. The total number of candidate
-strings is $$n*(n-1)*(n-2)*...*3*2*1$$, which is $$n!$$. Although some of
-the strings may be duplicates, the program cannot know this ahead of
-time and so it will still generate $$n!$$ different strings.
+A *brute force* technique for solving a problem typically tries to exhaust all
+possibilities. We can apply this technique to the anagram problem by generating
+a list of all possible strings using the characters from `s1`. If `s2` occurs
+in that list, then `s1` and `s2` ar anagrams.
+
+There is a difficulty with this approach, however: when generating all possible
+strings from `s1`, there are $$n$$ possible first characters, $$n-1$$ possible
+second characters, $$n-2$$ possible third characters, and so on. The total
+number of candidate strings is $$n*(n-1)*(n-2)*...*3*2*1$$, which is $$n!$$.
+Although some of the strings may be duplicates, the program cannot know this
+ahead of time, so it will still generate $$n!$$ strings.
 
 It turns out that $$n!$$ grows even faster than $$2^{n}$$ as *n* gets large.
-In fact, if `s1` were 20 characters long, there would be
-$$20!$$ or 2,432,902,008,176,640,000 possible candidate strings. If we
-processed one possibility every second, it would still take us
-77,146,816,596 years to go through the entire list. This is probably not
-going to be a good solution.
+If `s1` were 20 characters long, there would be $$20!$$ or
+2,432,902,008,176,640,000 possible candidate strings. If we processed one
+candidate every second, it would take 77,146,816,596 years to process the
+entire list. This is probably not going to be a good solution.
 
 Solution 4: Count and Compare
 -----------------------------
