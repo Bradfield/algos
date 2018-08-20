4705979f4fc4b5c7d9736685c3859b5013017142
Joshua Goller <jsgoller1@gmail.com>
2018-08-20 01:35:46 -0700

    Correct typo, "bellow" -> "below"

--- book/graphs/word_ladder.py
+++ book/graphs/word_ladder.py
@@ -39,7 +39,7 @@ Our first problem is to figure out how to turn a large collection of
 words into a graph. What we would like is to have an edge from one word
 to another if the two words are only different by a single letter. If we
 can create such a graph, then any path from one word to another is a
-solution to the word ladder puzzle. The illustration bellow shows a
+solution to the word ladder puzzle. The illustration below shows a
 small graph of some words that solve the FOOL to SAGE word ladder
 problem. Notice that the graph is an undirected graph and that the edges
 are unweighted.
